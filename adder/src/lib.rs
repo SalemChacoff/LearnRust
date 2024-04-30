8b1b89dd3cce5595f42fea697be0f786802c9c67
SalemChacoff <77862771+SalemChacoff@users.noreply.github.com>
2024-04-30 16:36:35 -0400

    rust testing-2

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -40,6 +40,11 @@ impl Guess {
     }
 }
 
+fn prints_and_returns_10(a: i32) -> i32 {
+    println!("I got the value {}", a);
+    10
+}
+
 #[cfg(test)]
 mod tests {
     // super:: is the parent module of the current module
@@ -107,6 +112,25 @@ mod tests {
             Err(String::from("Two plus two does not equal four"))
         }
     }
+
+    #[test]
+    fn this_test_will_pass() {
+        let value = prints_and_returns_10(4);
+        assert_eq!(10, value);
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        // code that takes an hour to run
+    }
+
+    /* 
+    #[test]
+    fn this_test_will_fail() {
+        let value = prints_and_returns_10(8);
+        assert_eq!(5, value);
+    }*/
 
     /* 
     #[test]
