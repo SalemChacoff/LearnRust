30ffae3ed01662c8c732e340453a57c21d2fcc92
SalemChacoff <77862771+SalemChacoff@users.noreply.github.com>
2024-05-03 20:54:53 -0400

    rust Iterators In Action (Using minigrep)

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,8 +5,9 @@ use minigrep::Config;
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+    //let args: Vec<String> = env::args().collect();
+    
+    let config: Config = Config::newIterator(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
